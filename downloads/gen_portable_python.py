46af85f055d86a420daef9144260c1b6d1895560
mdecycu <101738344+mdecycu@users.noreply.github.com>
2024-02-06 02:16:44 +0000

    說明如何配置可攜 Python 3.12.1 以及 portablegit

--- downloads/gen_portable_python.py
+++ downloads/gen_portable_python.py
@@ -1,30 +1,31 @@
-import urllib.request
-import os
+  import urllib.request
+  import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-# basic files for Python installation
-# before 3.12.0
-#py_list = ["core", "dev", "exe", "lib", "tcltk", "tools"]
-# 3.12.0 no tools.msi
-py_list = ["core", "dev", "exe", "lib", "tcltk"]
-# Python version
-version = "3.12.0"
-# Python msi download URL
-ftp = "https://www.python.org/ftp/python/" + version + "/amd64/"
-# location for Portable Python
-path = "y:\\tmp\\Python312"
-# create directory
-try:
-    os.mkdir(path)
-except:
-    # path exists
-    pass
-# get Python installation msi files and extract into target dir
-for i in py_list:
-    filename = i + ".msi"
-    url = ftp + filename
-    # download basic python msi file
-    urllib.request.urlretrieve(url, filename)
-    os.system("msiexec.exe /a " + i + ".msi targetdir=" + path)
-    # delete msi file
-    os.remove(dir_path + "\\" + i + ".msi")
+  dir_path = os.path.dirname(os.path.realpath(__file__))
+  # basic files for Python installation
+  # before 3.12.0
+  #py_list = ["core", "dev", "exe", "lib", "tcltk", "tools"]
+  # 3.12.1 no tools.msi
+  py_list = ["core", "dev", "exe", "lib", "tcltk"]
+  # Python version
+  version = "3.12.1"
+  # Python msi download URL
+  ftp = "https://www.python.org/ftp/python/" + version + "/amd64/"
+  # location for Portable Python
+  path = "y:\\tmp\\Python312"
+  # create directory
+  try:
+      os.mkdir(path)
+  except:
+      # path exists
+      pass
+  # get Python installation msi files and extract into target dir
+  for i in py_list:
+      filename = i + ".msi"
+      url = ftp + filename
+      # download basic python msi file
+      urllib.request.urlretrieve(url, filename)
+      os.system("msiexec.exe /a " + i + ".msi targetdir=" + path)
+      # delete msi file
+      os.remove(dir_path + "\\" + i + ".msi")
+      os.remove(path + "\\" + i + ".msi")
